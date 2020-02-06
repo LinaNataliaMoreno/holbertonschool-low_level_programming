8957d6c3a610465342f543effdb658f7d9728ae1
Natalia <tucarrera.co@gmail.com>
2020-02-06 19:52:56 +0000

    random number to the variable n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main -  Ramdom number, last digit <5, >5, =0.
+* srand - Pseudo-random number generator with seed
+* rand - Generate random numbers
+* printf - Print a messege
+* Return: 0
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
