c27e03eabe23a8b4d9926d2b432a7af58e232a4e
Natalia <tucarrera.co@gmail.com>
2020-02-08 01:10:39 +0000

    tasks until 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - while
+* putchar - prints all the numbers between 00 to 99
+* Return: 0
+*/
+int main(void)
+{
+	int i, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			putchar(i + '0');
+			putchar(m + '0');
+			if (!(i == 9 && m == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+putchar('\n');
+}
+
