eb7bce82bd7b913437653deb90239ac834d92716
Natalia <tucarrera.co@gmail.com>
2020-02-10 15:22:14 +0000

    combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+/**
+*main - display all possible combinations of three digits
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		j = i + 1;
+		for (; j <= 9; j++)
+		{
+			k = j + 1;
+			for (; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
