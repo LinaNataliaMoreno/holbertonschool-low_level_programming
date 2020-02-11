8b6e4eae94d57a7235995610df5983829186630b
Natalia <tucarrera.co@gmail.com>
2020-02-11 23:07:10 +0000

    Prototype: int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* _abs - check the code for Holberton School students.
+* @c : int
+* Return: Always 0.
+*/
+
+
+int _abs(int c)
+{
+	return (abs(c));
+}
