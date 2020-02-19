3640ca88a59b039a780b461291197f0a26a382d5
Natalia <tucarrera.co@gmail.com>
2020-02-19 18:06:55 +0000

    every other character of a string, starting with the first

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /** 
 * puts2 - prints every other character of a string, starting first character
-* @str : it addresses ti the string
+* @str: it is the input that is a string
+* Return: void
 */
+
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		_putchar(str[i]);
